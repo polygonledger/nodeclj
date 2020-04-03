878af96fe3c18bf8c9118905cb9f0ba0fb8578e6
benjyz <ben@enet.io>
2020-04-03 13:03:54 +0700

    wallet interop

--- src/nodeclj/core.clj
+++ src/nodeclj/core.clj
@@ -3,8 +3,13 @@
   ;(:require [nodeclj.bitcoin :as b])
   ;(:require [nodeclj.tcp :as t])
   (:require [nodeclj.othertcp :as t])
-  (:import 
+  (:import
    (org.bitcoinj.params MainNetParams)
+   (org.bitcoinj.core DumpedPrivateKey)
+   (org.bitcoinj.wallet DeterministicSeed)
+   (org.bitcoinj.wallet KeyChainGroupStructure)
+   (org.bitcoinj.wallet Wallet)
+   (org.bitcoinj.script Script)
    (java.net Socket)
    (java.io PrintWriter InputStreamReader BufferedReader)))
 
@@ -41,15 +46,28 @@
 (defn -main
   [& args]
   (println "run main")
-  ;(t/start server)
+  (let [p (MainNetParams/get)
+        seedCode "yard impulse luxury drive today throw farm pepper survey wreck glass federal"
+        passphrase ""
+        creationtime 1409478661
+        seed (new DeterministicSeed seedCode nil passphrase creationtime)
+        p2pkh nil ;Script/ScriptType/P2PKH
+        wallet (Wallet/fromSeed p seed)
+        ;pk (DumpedPrivateKey/fromBase58 p "xxxx")
+        ]
+    (println p)
+    (println seed wallet)
+    ;DumpedPrivateKey dumpedPrivateKey = DumpedPrivateKey.fromBase58 (params, args [0]);
+    )  
 
   ;(ntcl/serve nodeport)
   ;;(def a (serve-persistent 8888 #(.toUpperCase %)))
   ;(t/serve 8888)
   ;(b/create-keypair)
-
+  
   )
 
 
 
+;(t/start server)
 
